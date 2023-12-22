1e4b42ad81b892f50e95ffe563e72b1832c3f036
AndreaBevilacqua <andrebevi2000@gmail.com>
2023-12-22 19:13:12 +0100

    added second exercise

--- array-tail/js/tail.js
+++ array-tail/js/tail.js
@@ -8,3 +8,20 @@ console.log('JS OK')
  3 - chiediamo all'utente quanti elementi stampare
  4 - stampo gli elementi richiesti dall'utente
  */
+
+ const elementArray = parseInt(prompt("Quanti numeri desideri generare?"));
+
+ const randomNumbers = [];
+ for(let i = 0; i < elementArray; i++){
+    randomNumbers.push(Math.floor(Math.random() * 100) + 1);
+ }
+
+ console.table(randomNumbers);
+
+ const printElement  = parseInt(prompt("Quanti elementi vuoi visualizzare?"));
+ if (printElement > elementArray) {
+    console.log("Error");
+} else {
+    const lastElements = randomNumbers.slice(-printElement);
+    console.table(lastElements);
+}
