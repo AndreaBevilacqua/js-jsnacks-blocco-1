b6f17275daa06c69a01f1203e4e762d4df984c9b
AndreaBevilacqua <andrebevi2000@gmail.com>
2023-12-22 18:48:00 +0100

    added first todo

--- array-lista/js/frutta.js
+++ array-lista/js/frutta.js
@@ -1,1 +1,11 @@
 console.log('JS OK')
+
+// ESERCIZIO 1
+
+/* 
+ 1 - Creo un array degli ingredienti all'interno del frigo
+ 2 - aggiungo tramite push la pesca
+ 3 - verifico se é presente il cocomero 
+ 4 - Creo messeggio per comunicare la presenza del cocomero o se non c'é 
+*/
+
