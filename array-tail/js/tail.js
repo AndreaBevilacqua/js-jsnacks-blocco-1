0b651355b71e9390cf09fe01a5ee5d25271a227d
AndreaBevilacqua <andrebevi2000@gmail.com>
2023-12-22 19:02:57 +0100

    added 2 todo

--- array-tail/js/tail.js
+++ array-tail/js/tail.js
@@ -1,1 +1,10 @@
 console.log('JS OK')
+
+// ESERCIZIO 2
+
+/* 
+ 1 - Chiedo all'utente quanti numeri vuole
+ 2 - creo array con numeri compresi tra 1 e 100
+ 3 - chiediamo all'utente quanti elementi stampare
+ 4 - stampo gli elementi richiesti dall'utente
+ */
